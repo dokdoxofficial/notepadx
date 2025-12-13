1e0677566a7bb77f569802528c6f2feb3e130462
dokdox andy <dokdoxisgreat@gmail.com>
2025-12-14 06:38:27 +0900

    the new and simple error page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,10 @@
 export default function NotFound() {
   return (
-    <div style={{ padding: "50px", textAlign: "center" }}>
-      <h1>Error-404</h1>
-      <p>NotepadX-Not Found</p>
+    <div>
+      <h2>Error-404</h2>
+      <p>NotepadX-Not Found error:404</p>
+      <p>There is no information about the error.</p>
+      <a href="https://notepadx.netlify.app">visit Notepadx.neltify.app</a>
     </div>
   );
 }
