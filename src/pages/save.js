9c015edc07488e7de81d94866d2bab9a9f7a9043
dokdox andy <dokdoxisgreat@gmail.com>
2026-02-12 22:31:23 +0900

    new message when deleting memo or document

--- src/pages/save.js
+++ src/pages/save.js
@@ -15,8 +15,9 @@ export default function Save() {
       </Link>
 
       <main>
-        <h2>성공!</h2>
+        <h2>작성된 파일이 삭제됨.</h2>
         <p>성공적으로 삭제되었습니다.</p>
+        <a href='https://notepadx.netlify.app'>개발자</a>
         </main>
         
   
