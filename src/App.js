74cb888c72993d2463d484be277df338c3b30eea
developer_Andy <dokdoxisgreat@gmail.com>
2026-03-19 20:30:56 +0900

    add new save feature

--- src/App.js
+++ src/App.js
@@ -18,12 +18,7 @@ useEffect(() => {
 
 useEffect(() => {
   localStorage.setItem("textareadata", data);
-  if (data === "help"){
-       setfilestate("도움말을 확인할려면 느낌표버튼을 클릭해보세요.ntx 를 입력해서 버전이름을 확인해보세요.")
-  }
-  else if (data === "ntx"){
-       setfilestate("NotepadX v10-integrated Deployed under Mit licence except the icon")
-  }else if (data !==""){
+  if (data !==""){
        setfilestate("저장되었습니다.새로운 변동사항이 자동으로 저장됩니다.")
   } 
 }, [data]);
@@ -52,7 +47,10 @@ const askGemini = async () => {
   setData(text + data)
 };
 
-
+function save(){
+    setfilestate("저장할려면 set as pdf 또는 pdf 로 저장을 클릭하고 save 버튼을 클릭하세요.")
+    window.print()
+}
   return (
     <>
     <h1>NotepadX</h1>
@@ -61,7 +59,8 @@ const askGemini = async () => {
        <button><i class="fa-solid fa-minus" onClick={()=>settextsize(textsize-10)}></i></button>
        <button onClick={()=>window.print()}><i class="fa-solid fa-print"></i></button>
        <button><a id = "info" href='#footer'><i class="fa-regular fa-circle-question"></i></a></button>
-       <button onClick={askGemini}>작성된 내용을 바탕으로 글쓰기</button>
+       <button onClick={save}><i class="fa-solid fa-down-long"></i></button>
+       <button onClick={askGemini}>작성된 내용으로 ai글쓰기</button>
        <div id='state'>{filestate}</div>
     </div>
     <main>
@@ -76,9 +75,9 @@ const askGemini = async () => {
          </h4>
          <h3>NotepadX 는 무료인가요?</h3>
          <h4>NotepadX 는 완전히 무료입니다.심지어 오픈소스 기반 프로젝트로써 여러 프라이버시를 보호합니다.NotepadX에 입력한 모든 내용은 임력한 디바이스에 저장되므로
-            개인정보가 근본적으로 보호됩니다.또한 개인정보 보호 약관을 확인할수도 있습니다.<a classname = "greenlink" href='https://notepadxprivacy.netlify.app'>여기를 클릭해서 확인해보세요.</a>
+            개인정보가 근본적으로 보호됩니다.또한 개인정보 보호 약관을 확인할수도 있습니다.<a className = "greenlink" href='https://notepadxprivacy.netlify.app'>여기를 클릭해서 확인해보세요.</a>
          </h4>
-        <h5>V10-Integrated Version</h5>
+        <h5>NotepadX v10-integrated Deployed under Mit licence except the icon</h5>
         <a className = "greenlink" href="https://www.flaticon.com/free-icons/notepad" title="notepad icons">Notepad icons created by Freepik - Flaticon</a>
     </footer>
     </>
